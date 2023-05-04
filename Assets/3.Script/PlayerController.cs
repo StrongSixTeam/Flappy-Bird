807b7274c8387cffefaa610ce41c653b609a53e1
y00nhye <109010026+y00nhye@users.noreply.github.com>
2023-05-04 13:15:02 +0900

    1
    playercontroller
    - velocity
    - rotation

---
+++ Assets/3.Script/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody player_R;
+
+    //left, right wing
+    [SerializeField] Transform[] Wings;
+
+    private void Awake()
+    {
+        TryGetComponent(out player_R);
+    }
+    private void Update()
+    {
+        //마우스 버튼으로 위쪽 방향 힘 주기
+        if (Input.GetMouseButtonDown(0))
+        {
+            player_R.velocity = new Vector3(0, 2, 0);
+        }
+
+        //힘에 따라 캐릭터 로테이션 돌리기
+        if (player_R.velocity.y > 0)
+        {
+            transform.Rotate(new Vector3(50f * Time.deltaTime, 0, 0));
+        }
+        if (player_R.velocity.y <= 0)
+        {
+            transform.Rotate(new Vector3(-50f * Time.deltaTime, 0, 0));
+        }
+    }
+}
